b9048273afcaee321455938db7482ed631cd9afe
Javier Heisecke <javierheiseckee@gmail.com>
2025-02-03 17:27:33 -0300

    init project

---
+++ AnotherCatsApp/AnotherCatsApp/AnotherCatsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnotherCatsAppApp.swift
+//  AnotherCatsApp
+//
+//  Created by Javier Heisecke on 2025-02-03.
+//
+
+import SwiftUI
+
+@main
+struct AnotherCatsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
