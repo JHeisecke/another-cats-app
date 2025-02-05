02f619d5b944e6a193f073c6b7a0cc757d1e3d69
Javier Heisecke <javierheiseckee@gmail.com>
2025-02-04 23:09:47 -0300

    [Fix] Delete SceneDelegate from AppDelegate

--- AnotherCatsApp/AnotherCatsApp/Core/Delegates/AppDelegate.swift
+++ AnotherCatsApp/AnotherCatsApp/Core/Delegates/AppDelegate.swift
@@ -14,14 +14,4 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
         return AppDelegate.orientationLock
     }
-
-    func application(
-        _ application: UIApplication,
-        configurationForConnecting connectingSceneSession: UISceneSession,
-        options: UIScene.ConnectionOptions
-    ) -> UISceneConfiguration {
-        let sceneConfig = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
-        sceneConfig.delegateClass = SceneDelegate.self
-        return sceneConfig
-    }
 }
